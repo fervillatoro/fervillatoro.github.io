06c56ccbad5575f3d5f457a6c53e4ebd6e963577
Alejandro Villatoro <alejandrov.afervm@gmail.com>
2021-08-19 13:12:57 -0600

    Add SW

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,34 @@ let villa = {
     }
   }
 };
+
+navigator.serviceWorker.register('/sw.min.js')
+.then(function(registration) {
+
+  registration.addEventListener('updatefound', () => {
+    // se encontro una nueva version de SW y se actualizo
+    console.log("Se actualizo el SW a la nueva version.");
+
+    // Un trabajador de servicio salvaje ha aparecido en reg.installing!
+    const newWorker = registration.installing;
+
+    newWorker.state;
+    // "installing" - el evento de instalación se ha activado, pero aún no se ha completado
+    // "installed"  - instalacion completada
+    // "activating" - el evento de activación se ha disparado, pero aún no se ha completado
+    // "activated"  - completamente activo
+    // "redundant"  - descartado. Se produjo un error en la instalación o se reemplazó por una versión más reciente.
+
+    newWorker.addEventListener('statechange', () => {
+      // newWorker.state ha cambiado
+      console.log("El evento statechange ha cambiado");
+    });
+  });
+  console.log(registration);
+  // El registro funcionó
+  console.log('Registration succeeded.');
+})
+.catch(function(error) {
+  // el registro falló
+  console.log('Registration failed with ' + error);
+});
---
+++ sw.js
@@ -0,0 +1,53 @@
+/*! Copyright (c) 2021 Alejandro Villatoro. All Rights Reserved. */
+
+let CACHE_VERSION = "v1";
+let viewOffline = '/';
+
+let CACHE_ASSETS = [
+  '/favicon.ico',
+  'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css',
+  '/assets/css/index.min.css',
+  '/assets/js/index.min.js',
+  '/assets/img/avatar2.jpg',
+  '/assets/img/avatar.png',
+  '/assets/img/fondo-bio.jpg',
+  '/assets/img/fondo-contact.jpg',
+  // experiencia offline
+  viewOffline
+];
+
+self.addEventListener('install', function(event) {
+if ( self.skipWaiting ) { self.skipWaiting(); }//instalar de inmediato
+  event.waitUntil(
+    caches.open( CACHE_VERSION ).then(function(cache) {
+      return cache.addAll( CACHE_ASSETS );
+    })
+  );
+});
+
+//Cache, falling back to network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response ||
+      fetch(event.request)
+      .catch(err => caches.open( CACHE_VERSION ).then( cache => cache.match(viewOffline) ) )
+    })
+  );
+});
+var cacheWhitelist = [ CACHE_VERSION ];
+caches.keys().then(function(cacheNames) {
+  return Promise.all(
+    cacheNames.map(function(cacheName) {
+      if (cacheWhitelist.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+    })
+  );
+});
+
+caches.keys().then(function(cacheKeys) {
+  console.log('Versión SW: '+cacheKeys);
+});
+
+// importScripts('https://cdn.webpushr.com/sw-server.min.js');
