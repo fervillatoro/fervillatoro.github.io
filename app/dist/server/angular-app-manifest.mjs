bfda75dcc4902fc0a71a57fdc3b96b854ea99cd0
Alejandro Villatoro <alejandrov.afervm@gmail.com>
2026-05-07 21:25:27 -0500

    Mejoras

--- app/dist/server/angular-app-manifest.mjs
+++ app/dist/server/angular-app-manifest.mjs
@@ -22,9 +22,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 26447, hash: 'b2d20846ee78c4b81a0ca9d07fc739d361e4ad55dfb1144808747f76b8ccad13', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 22205, hash: '8817ffd1cdc19ef92586ba329f028a6878148cc29b65e1546ac9d7dc9ea6fde4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 32327, hash: '262143870e7fd81e514ad7b3af148e4500a8e9fb738cd04026ecb28309be6ac6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 26456, hash: '3dc9a17d1c28892a4807967a0514d58e15e5246a6b84275e0e54c966b4a33613', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 22214, hash: '7507e4816d5f324c21548c7fecf19ae3c036308958482119a8222b9305c11ffb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 32336, hash: 'df0abdf051d4000f6081a0992323ebb8a48012cfbb3841e4b05569bbe010b58c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-JWU4A3JI.css': {size: 232366, hash: 'WA3C/+u1hcI', text: () => import('./assets-chunks/styles-JWU4A3JI_css.mjs').then(m => m.default)}
   },
 };
